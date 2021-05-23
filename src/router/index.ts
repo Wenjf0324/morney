922320900d127e5144b6dad9ea5d26e26445d7e6
八宝粥 <1099004091@qq.com>
2021-05-23 11:20:38 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from "@/views/Home.vue";
 import Money from "@/views/Money.vue";
 import Labels from "@/views/Labels.vue";
 import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,11 @@ const routes = [
   {
     path: "/statistics",
     component: Statistics,
+  },
+  //其他路径为404页面
+  {
+    path: "*",
+    component: NotFound,
   },
 ];
 
