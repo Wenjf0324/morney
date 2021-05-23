ed59ac75af8b8de8d3fda532922653031bc21410
八宝粥 <1099004091@qq.com>
2021-05-23 11:47:03 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,14 +1,96 @@
 <template>
-  <div>
-    <div>Money</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+      <p>Money</p>
+    </div>
     <Nav />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
 export default {};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.nav-wrapper {
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1; //将剩余空间尽量分配给content容器，实现导航栏始终在底部
+}
+</style>
