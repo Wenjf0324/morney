a3bcc3844282ef7ddb3f7f493edf68a96016a2df
八宝粥 <1099004091@qq.com>
2021-06-08 16:48:16 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -9,13 +9,15 @@ type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
   create: (name: string) => "success" | "duplicated"; //联合类型。success 表示成功，duplicated 表示name重复
+  update: (id: string, name: string) => "success" | "not found" | "duplicated";
+  remove: (id: string) => boolean;
   save: () => void;
 };
 
 const tagListModel: TagListModel = {
   data: [],
 
-  //获取数据
+  //获取所有标签名
   fetch() {
     this.data = JSON.parse(
       window.localStorage.getItem(localStorageKeyName) || "[]"
@@ -23,6 +25,7 @@ const tagListModel: TagListModel = {
     return this.data;
   },
 
+  //添加新的标签
   create(name: string) {
     //this.data = [{id:'1', name:'1'}, {id:'2', name:'2'}]
     const names = this.data.map((item) => item.name);
@@ -32,6 +35,42 @@ const tagListModel: TagListModel = {
     this.data.push({ id: name, name: name });
     this.save();
     return "success";
+  },
+
+  //更新标签名
+  update(id, name) {
+    const idList = this.data.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      //id存在
+      const names = this.data.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        //name标签名重复
+        return "duplicated";
+      } else {
+        //更新标签名
+        const tag = this.data.filter((item) => item.id === id)[0];
+        tag.name = name;
+        this.save();
+        return "success";
+      }
+    } else {
+      //id不存在
+      return "not found";
+    }
+  },
+
+  //删除标签
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+      }
+      break;
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
 
   //保存数据
