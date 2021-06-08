febb1e82daeabfd7dd47db9b001383cf95c208c4
八宝粥 <1099004091@qq.com>
2021-06-08 19:01:34 +0800

    重新封装 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -3,19 +3,21 @@ import { RecordItem } from "../custom";
 const localStorageKeyName = "recordList";
 
 const recordListModel = {
+  data: [] as RecordItem[],
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
   },
   //获取数据
   fetch() {
-    return JSON.parse(
+    this.data = JSON.parse(
       window.localStorage.getItem(localStorageKeyName) || "[]"
     ) as RecordItem[]; //强制指定为 RecordItem 类型
+    return this.data;
   },
   //保存数据
-  save(data: RecordItem[]) {
+  save() {
     //JSON.stringify()序列化，转化成数组
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
 };
 
